dd1d7b31c71514402be78a7bf81e263a60bb6578
shivamlekhwar98 <shivamlekhwar98@gmail.com>
2025-09-06 07:51:51 +0000

    modified string calculator to support \n in numbers string

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,6 +1,6 @@
 class StringCalculator
     def self.add(num)
-        numbers = num.split(",")
+        numbers = num.split(/[\n,]/)
         sum = 0
         numbers.each do |num|
             sum = sum + num.to_i
