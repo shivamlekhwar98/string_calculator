f2cfe044d42ed2aac5358be83810d5a962da6012
shivamlekhwar98 <shivamlekhwar98@gmail.com>
2025-09-06 11:00:03 +0000

    added comments

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,18 +1,24 @@
 class StringCalculator
     def self.add(numbers)
+        # empty checking numbers
         return 0 if numbers.empty?
 
+        # checking case for numbers starting with //
         if numbers.start_with?("//")
             delimiter_line, numbers_part = numbers.split("\n", 2)
             delimiter = delimiter_line[2..]
             delimiters = Regexp.new(Regexp.escape(delimiter))
+            # splitting numbers with specific delimitor after //
             numbers = numbers_part.split(delimiters)
         else
+            # splitting numbers with \n or ,
             numbers = numbers.split(/[\n,]/)
         end
 
+        # filtering negative numbers
         negative_numbers = numbers.select{|n| n.to_i < 0 }
 
+        # if negative numbers are empty, we return the sum of numbers else we raise Argument Error
         if negative_numbers.empty?
             return numbers.map(&:to_i).sum
         else
