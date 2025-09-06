e967a56d8111064d23b90fc2efd471d74297b8b5
shivamlekhwar98 <shivamlekhwar98@gmail.com>
2025-09-06 10:52:58 +0000

    refactored code, updated passed param as numbers as per the the requirement

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,14 +1,14 @@
 class StringCalculator
-    def self.add(num)
-        return 0 if num.empty?
+    def self.add(numbers)
+        return 0 if numbers.empty?
 
-        if num.start_with?("//")
-            delimiter_line, numbers_part = num.split("\n", 2)
+        if numbers.start_with?("//")
+            delimiter_line, numbers_part = numbers.split("\n", 2)
             delimiter = delimiter_line[2..]
             delimiters = Regexp.new(Regexp.escape(delimiter))
             numbers = numbers_part.split(delimiters)
         else
-            numbers = num.split(/[\n,]/)
+            numbers = numbers.split(/[\n,]/)
         end
 
         negative_numbers = numbers.select{|n| n.to_i < 0 }
