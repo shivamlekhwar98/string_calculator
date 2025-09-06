ed4e2aa50c913b9538d7303ab078a02313247f98
shivamlekhwar98 <shivamlekhwar98@gmail.com>
2025-09-06 10:15:08 +0000

    modified string_calculator class to support different deliminitors

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,6 +1,14 @@
 class StringCalculator
     def self.add(num)
-        numbers = num.split(/[\n,]/)
+        if num.start_with?("//")
+            delimiter_line, numbers_part = num.split("\n", 2)
+            delimiter = delimiter_line[2..]
+            delimiters = Regexp.new(Regexp.escape(delimiter))
+            numbers = numbers_part.split(delimiters)
+        else
+            numbers = num.split(/[\n,]/)
+        end
+
         sum = 0
         numbers.each do |num|
             sum = sum + num.to_i
